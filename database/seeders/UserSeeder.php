29e1cd8f0b6d01a466b8bad22a925efc7910a602
asihagtina <asihagtina44@gmail.com>
2026-08-19 14:08:02 +0700

    modify the place a photos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,7 +11,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'punyadmin@gothicclothing.com'],
+            ['email' => 'punyadmin@gothicclothing.com'], 
             [
                 'name'     => 'Admin',
                 'role'     => 'admin', // Menandai akun ini sebagai Admin
